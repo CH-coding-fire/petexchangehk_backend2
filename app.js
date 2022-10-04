9385b8b883ab98020f96307635ddff97178c4659
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-10-04 17:24:40 +0800

    header allow-methods

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ app.use(bodyParser.text());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', targetClientURL);
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
   next();
 });
 
